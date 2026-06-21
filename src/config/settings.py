809c6898ef5bfded1febd90a928f6854d4e2bbe4
Siva <sivasankarmutte@gmail.com>
2026-06-21 11:55:22 +0530

    made env vars as None in settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -11,14 +11,14 @@ class Settings(BaseSettings):
     DATABASE_URL: str | None = None
     REDIS_URL: str | None = None
 
-    POSTGRES_HOST: str
-    POSTGRES_PORT: int
-    POSTGRES_DB: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str | None = None
+    POSTGRES_PORT: int | None = None
+    POSTGRES_DB: str | None = None
+    POSTGRES_USER: str | None = None
+    POSTGRES_PASSWORD: str | None = None
 
-    REDIS_HOST: str
-    REDIS_PORT: int
+    REDIS_HOST: str | None = None
+    REDIS_PORT: int | None = None
 
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str
